c007648755f1859e8f794c24dd2f80b90f461761
Steven Gardner <gemaojing1234@126.com>
2014-12-18 14:20:40 +0800

    Initial Commit

---
+++ barcodeGMJ001/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  barcodeGMJ001
+//
+//  Created by admin on 14/12/18.
+//  Copyright (c) 2014年 葛茂菁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
